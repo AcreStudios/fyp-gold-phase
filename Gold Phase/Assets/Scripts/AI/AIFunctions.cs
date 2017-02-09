d577155ff749647a3a88c8cd4df390d47264d5ca
zlurker <tallspin_97@hotmail.com>
2017-02-09 09:55:13 +0800

    Importing of Scripts
    -Got rid of obsolete scripts.
    -Waiting for gavin to import all the prefabs/ect before I start
    importing the AI Testing Scene.

---
+++ Gold Phase/Assets/Scripts/AI/AIFunctions.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIFunctions : MonoBehaviour {
+
+    public enum CoverType {
+        None, High, Low
+    }
+    [Header("Cover Settings")]
+    public Transform minHeightForCover;
+    public Transform maxHeightForCover;
+    public float durationInCover;
+    public bool ableToHide;
+
+    [Header("Target-Related Settings")]
+    public Vector3 destination;
+    public Collider destinationMarker;
+    public Transform target;
+    protected Transform[] guns = new Transform[1];
+    protected Transform linecastCheck;
+    protected Vector3 startingPoint;
+    protected bool showGunEffect;
+    protected Animator animator;
+    protected UnityEngine.AI.NavMeshAgent agent;
+    public CoverType coverType;
+    //protected Vector3 
+    //Health hpScript;
+
+    void Awake() {
+        //hpScript = GetComponent<Health>();
+    }
+
+    public virtual void DamageRecieved() {
+        if (!CivillianManager.instance.hostile)
+            CivillianManager.instance.AISetToHostile();
+
+        // if (hpScript.curHealth <= 0)
+        // enabled = false;
+    }
+
+    public IEnumerator ChangeObjectLocation(GameObject obj, Vector3 location) {
+        yield return new WaitForSeconds(0.1f);
+        obj.transform.position = location;
+    }
+
+    public Vector3 GetDestinationPoint(float range) {
+        if (ableToHide) {
+            Vector3 tempGradient = target.position - transform.position;
+            tempGradient = Mathf.Abs(tempGradient.x) >= Mathf.Abs(tempGradient.z) ? tempGradient / Mathf.Abs(tempGradient.x) : tempGradient / Mathf.Abs(tempGradient.z);
+            tempGradient.y = 0;
+
+            for (var j = -1; j < 2; j++) {
+                if (j != 0) {
+                    //Debug.DrawLine(target.position, target.position + (tempGradient * -(range / 4)) + ((new Vector3(-(tempGradient.z), 0, tempGradient.x) * range / 4) * j), Color.black, 5);
+                    Collider[] colliders = Physics.OverlapSphere(target.position + (tempGradient * -(range / 4)) + ((new Vector3(-(tempGradient.z), 0, tempGradient.x) * range / 4) * j), range / 2);
+
+                    for (var i = 0; i < colliders.Length; i++) {
+                        if (colliders[i].transform.CompareTag("Untagged"))
+                            if (colliders[i].bounds.center.y + colliders[i].bounds.extents.y > minHeightForCover.position.y) {
+                                Vector3 temp = Vector3.zero;
+                                int vectorAffected;
+                                float maxValue;
+
+                                temp = colliders[i].bounds.center - target.position;
+
+                                temp.x = (temp.x / Mathf.Abs(temp.x)) * colliders[i].bounds.extents.x;
+                                temp.z = (temp.z / Mathf.Abs(temp.z)) * colliders[i].bounds.extents.z;
+
+                                if (colliders[i].bounds.center.y + colliders[i].bounds.extents.y > maxHeightForCover.position.y) {
+                                    coverType = CoverType.High;
+                                    if (Mathf.Abs(colliders[i].bounds.center.x - target.position.x) > Mathf.Abs(colliders[i].bounds.center.z - target.position.z))
+                                        temp.x *= -1;
+                                    else
+                                        temp.z *= -1;
+
+                                    vectorAffected = 0;
+                                    maxValue = temp[vectorAffected] + 0.5f;
+                                } else {
+                                    coverType = CoverType.Low;
+                                    if (Mathf.Abs(colliders[i].bounds.center.x - target.position.x) < Mathf.Abs(colliders[i].bounds.center.z - target.position.z))
+                                        vectorAffected = 0;
+                                    else
+                                        vectorAffected = 2;
+
+                                    maxValue = colliders[i].bounds.extents[vectorAffected];
+                                }
+
+
+                                for (var k = -maxValue; k <= maxValue; k++) { //-ve +ve is the problem need some fixes
+                                    //Debug.Log(k);
+                                    Vector3 cachedVector = new Vector3();
+                                    cachedVector = temp;
+                                    cachedVector[vectorAffected] = k;
+                                    cachedVector += colliders[i].bounds.center;
+                                    cachedVector.y = transform.position.y;
+                                    Debug.DrawLine(target.position, cachedVector, Color.red, 1);
+                                    // if (CheckIfPosAvail(cachedVector))
+                                    //return cachedVector;
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        coverType = CoverType.None;
+        return ArcBasedPosition(target.position - transform.position, target.position, range);
+    }
+
+    public Vector3 ArcBasedPosition(Vector3 givenVector, Vector3 targetPos, float givenLength) {
+        givenVector.y = 0;
+        targetPos.y = transform.position.y;
+
+        Vector3 gradient = Mathf.Abs(givenVector.x) >= Mathf.Abs(givenVector.z) ? givenVector / Mathf.Abs(givenVector.x) : givenVector / Mathf.Abs(givenVector.z);
+
+        for (var i = -givenLength; i < givenLength + 1; i++) {
+            Vector3 currentPosInCircle = gradient * i;
+            Vector3 reflexedGradient = new Vector3(-(gradient.z), 0, gradient.x) * (givenLength - Mathf.Abs(i));
+
+            if (CheckIfPosAvail(targetPos + (Vector3.Normalize(currentPosInCircle - reflexedGradient) * givenLength)))
+                return targetPos + (Vector3.Normalize(currentPosInCircle - reflexedGradient) * givenLength);
+
+            if (CheckIfPosAvail(targetPos + (Vector3.Normalize(currentPosInCircle + reflexedGradient) * givenLength)))
+                return targetPos + (Vector3.Normalize(currentPosInCircle + reflexedGradient) * givenLength);
+        }
+        return transform.position;
+    }
+
+    public bool CheckIfPosAvail(Vector3 temp) {
+        temp.y = transform.position.y;
+        bool hitFloor = false;
+
+        Collider[] inCollision = Physics.OverlapCapsule(temp, temp, 1);
+
+        foreach (Collider collision in inCollision) {
+            if (collision != destinationMarker)
+                if (collision.transform.tag == "Marker")
+                    return false;
+
+            if (collision.transform.tag == "Floor")
+                hitFloor = true;
+        }
+
+        if (hitFloor)
+            destinationMarker.transform.position = temp;
+
+        return hitFloor;
+    }
+}
---
+++ Gold Phase/Assets/Scripts/AI/CivillianAI.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CivillianAI : AIFunctions {
+
+    public enum Actions {
+        CheckForTask, PerformTask, Walk, PrepareForWalk, WaitingForQuery, RunFromPlayer
+    }
+
+    public Actions actions;
+
+    [Range(0, 1)]
+    public float talkativity = 0.5f; //slider value  
+    //[Range(0, 1)]
+    //public float socialbility = 0.5f;
+    public float attentionSpan = 5;
+    public float sightRange = 10;
+    public float fearRandomiserMin = 2;
+    public float fearRandomiserMax = 5;
+    public bool randomlyGeneratedValues;
+
+    CivillianManager.TaskLocation instance;
+    CivillianAI civillianTarget;
+    float timer;
+    int currentTaskUser;
+
+    Renderer colorChanging;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+
+        colorChanging = GetComponent<Renderer>();
+        CivillianManager.instance.civillianAIList.Add(this);
+        gameObject.tag = "Civillian";
+        gameObject.name = (CivillianManager.instance.civillianAIList.Count - 1).ToString();
+        agent = GetComponent<NavMeshAgent>();
+        instance = new CivillianManager.TaskLocation();
+
+        if (randomlyGeneratedValues) {
+            talkativity = Random.value;
+            //socialbility = Random.value;
+            attentionSpan = Random.Range(5, 60);
+        }
+
+        if (CivillianManager.instance.hostile)
+            Scared();
+    }
+
+    void Update() {
+        switch (actions) {
+            case Actions.CheckForTask:
+                if (timer <= Time.time) {
+                    FindNewTask(instance, currentTaskUser);
+
+                    if (instance.taskLocation) {
+                        target = instance.taskLocation;
+                        destination = ArcBasedPosition(target.position - transform.position, target.position, 2f);
+                        actions = Actions.PrepareForWalk;
+                    } else if (Random.value <= talkativity)
+                        FindSomeoneToTalkTo();
+                }
+                animator.SetInteger("TreeState", 0);
+                break;
+
+            case Actions.PerformTask:
+                transform.LookAt(target);
+                animator.SetInteger("TreeState", 0);
+
+                if (timer <= Time.time) {
+                    actions = Actions.CheckForTask;
+                    colorChanging.material.color = Color.blue;
+                    target = null;
+                }
+                break;
+
+            case Actions.PrepareForWalk:
+                agent.destination = destination;
+                animator.SetInteger("TreeState", 0);
+
+                if (destination == transform.position) {
+                    target = null;
+                    FindSomeoneToTalkTo();
+                }
+
+                if (agent.velocity.sqrMagnitude > 0 || (destination - transform.position).sqrMagnitude < 5)
+                    actions = Actions.Walk;
+                break;
+
+            case Actions.Walk:
+                transform.LookAt(destination);
+                animator.SetInteger("TreeState", 1);
+
+                if (agent.velocity.sqrMagnitude == 0) {
+                    actions = Actions.PerformTask;
+                    timer += Time.time;
+                    colorChanging.material.color = Color.green;
+
+                    if (civillianTarget) {
+                        civillianTarget.actions = Actions.PerformTask;
+                        civillianTarget.timer += Time.time;
+                        //Debug.LogFormat("{0} is talking to {1}", gameObject.name, civillianTarget.name);
+                        colorChanging.material.color = Color.red;
+                    }
+                }
+                break;
+
+            case Actions.RunFromPlayer:
+
+                if ((target.position - destination).sqrMagnitude < (sightRange * 10) * (sightRange * 10)) {
+                    destination = ArcBasedPosition(target.position - transform.position, target.position, sightRange * 10);
+                    agent.destination = destination;
+                }
+
+                if (agent.velocity.sqrMagnitude > 0)
+                    animator.SetInteger("TreeState", 1);
+                else
+                    animator.SetInteger("TreeState", 0);
+                break;
+        }
+        destinationMarker.transform.position = destination;
+    }
+
+    public void FindNewTask(CivillianManager.TaskLocation inst, int taskUser) {
+        instance = CivillianManager.instance.TaskQuery(gameObject, out timer, out currentTaskUser);
+
+        if (inst.taskLocation)
+            inst.civillianOnTask[taskUser] = null;
+    }
+
+    public void FindSomeoneToTalkTo() {
+        Collider[] inRadius = Physics.OverlapSphere(transform.position, sightRange);
+
+        foreach (Collider stuff in inRadius)
+            if (stuff.transform.CompareTag("Civillian") && stuff.gameObject != gameObject)
+                if (CivillianManager.instance.civillianAIList[int.Parse(stuff.name)].Talk(this))
+                    return;
+    }
+
+    public bool Talk(CivillianAI query) {
+        if (Random.value <= talkativity && !target) {
+            //To handle AI being queried.
+            target = query.transform;
+            destination = ArcBasedPosition(new Vector3(1, 0, 0), transform.position, 2);
+            agent.destination = destination;
+            timer = attentionSpan;
+            actions = Actions.WaitingForQuery;
+            colorChanging.material.color = Color.red;
+
+            //To handle the other AI querying.
+            query.timer = attentionSpan;
+            query.target = transform;
+            query.destination = ArcBasedPosition(transform.position - query.transform.position, transform.position, 2);
+            query.actions = Actions.PrepareForWalk;
+            query.civillianTarget = this;
+            return true;
+        }
+        return false;
+    }
+
+    public void Scared() {
+        actions = Actions.RunFromPlayer;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent.speed *= 2;
+    }
+
+    public override void DamageRecieved() {
+        base.DamageRecieved();
+    }
+}
