4fd937691f015ac13c569a67810928f9244ee06b
Aweife <aweifeisaweife@gmail.com>
2017-02-21 16:00:10 +0800

    Solving conflict pt 1
    Deleted everything in the unity proj, will import gab's stuff ltr

--- Gold Phase/Assets/Scripts/Events/EventData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class EventData  {
-
-    public static List<EventTree> eventTrees = new List<EventTree>();
-}
